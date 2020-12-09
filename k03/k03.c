0cbcb498fbe9c34abded2d74513c806c93947d4c
Eito Yokoyama <bb35319056@ms.nagasaki-u.ac.jp>
2020-12-09 11:16:03 +0900

    k03提出1回目

--- k03/k03.c
+++ k03/k03.c
@@ -9,14 +9,77 @@ char StrKey[] = "wind";
 
 char* ForceSearch(char text[], char key[])
 {
-    //  ここを実装する
+    int start, pos;
+    int text_len, key_len;
 
+    text_len = strlen(text);
+    key_len = strlen(key);
+
+    for(start = 0; start+key_len < text_len; start++)
+    {
+        for(pos = 0; pos < key_len; pos++)
+        {
+            if(text[start+pos] == key[pos])
+            {
+                if(pos+1 == key_len)
+                {
+                    return &text[start];
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    return NULL;
 }
 
 char* BMSearch(char text[], char key[])
 {
-    //  ここを実装する
+    char* ch;
+    int index, key_index;
+    int text_len, key_len;
+    int* table[256];
 
+    text_len = strlen(text);
+    key_len = strlen(key);
+    
+    for(int pos = 0; pos < 256; pos++)
+    {
+        table[pos] = key_len;
+    }
+    for(int pos = 0; key[pos] != NULL; pos++)
+    {
+        table[key[pos]] = (key_len-1) - pos;
+    }
+
+    key_index = key_len - 1;
+    while(text[key_index] != NULL)
+    {
+        ch = &text[key_index - (key_len-1)];
+        for(index = 0; key[index] != 0; index++)
+        {
+            if(text[key_index - index] != key[(key_len-1) - index])
+            {
+                if(table[text[key_index - index]] > index)
+                {
+                    key_index += (int)table[text[key_index - index]] - index;
+                }
+                else
+                {
+                    key_index++;
+                }
+                ch = NULL;
+                break;
+            }
+        }
+        if(ch != NULL)
+        {
+            return ch;
+        }
+    }
+    return NULL;
 }
 
 int main(void)
